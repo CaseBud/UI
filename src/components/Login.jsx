ea8eddb36216d04191b0b463618c6d67e1408ba1
Murewa Ajala <murewatajala@gmail.com>
2025-01-31 01:03:17 +0100

    Push

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -90,9 +90,9 @@ const Login = () => {
         <div className="relative z-10 flex flex-col h-full">
           <div className="flex items-center space-x-3">
             <div className="h-10 w-10 bg-white rounded-lg flex items-center justify-center">
-              <svg className="h-6 w-6 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-              </svg>
+                <svg className="h-6 w-6 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                </svg>
             </div>
             <h2 className="text-2xl font-bold text-white">CaseBud</h2>
           </div>
