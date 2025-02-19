829740205c5d38e7365bfc70cab79956a4c85f45
Oluwole <oluwoleafolabi0@gmail.com>
2025-02-19 23:40:34 +0100

    fix: revert typing animation

--- src/components/TypingAnimation.jsx
+++ src/components/TypingAnimation.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 
 const TypingAnimation = () => {
-    const texts = ["Thinking", "Processing", "Gathering data", "Generating responses", "Comparing responses", "Finalizing"];
-    const [currentText, setCurrentText] = React.useState(0);
-
-    React.useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentText((prev) => (prev + 1) % texts.length);
-        }, 5000);
-        return () => clearInterval(interval);
-    }, []);
-
     return (
         <div className="flex space-x-1">
             <div className="w-2 h-2 bg-slate-400 rounded-full animate-bounce"></div>
